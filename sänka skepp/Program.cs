8369c8baca08be9c87a552f7d46d283a1ef4a656
Arvid Petersson Lindberg <arvid.peterssonlind@learnet.se>
2020-10-14 15:06:14 +0200

    cfy

---
+++ sänka skepp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace sänka_skepp 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool färdig = false;
+
+            while(färdig == false)
+            {
+                Spel();
+
+                Console.WriteLine("Vill du spela igen? " + "Skriv R");
+
+                string restart = Console.ReadLine();
+
+                if (restart == "R")
+                {
+                    Spel();
+                }
+            }
+
+        }
+
+        static void Spel()
+        {
+            Console.WriteLine("VÄLKOMMEN TILL SÄNKA SKEPP!");
+
+            bool klar = false;
+
+            int koordinat = 16;
+
+            while(klar == false)
+            {
+            Console.WriteLine("Skriv in en koordinat " + "[1-20]");
+
+            int försök = int.Parse(Console.ReadLine());
+
+            if (försök == koordinat)
+            {
+                Console.WriteLine("Hit!");
+
+                klar = true;
+            }
+
+            else
+            {
+                Console.WriteLine("Miss!");
+            }
+
+        }
+    }
+} 
+}
